7ed3cda3e69da23f3978df4b110131f29f1638e6
Matías Bustos <agustinbustos_com@hotmail.com>
2023-08-02 23:05:37 +0200

    Adds bouncing-ball exercise

---
+++ 01-perlin-noise/bouncing-ball/sketch.js
@@ -0,0 +1,15 @@
+let xOffset = 0;
+let yOffset = 1000;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  xOffset+=0.01;
+  yOffset+=0.01;
+  const x = noise(xOffset) * width;
+  const y = noise(yOffset) * height;
+  background(51);
+  ellipse(x, y, 24, 24);
+}
