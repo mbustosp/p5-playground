5291f79d19dbbb5a9c29fd64d6edfbb32d1861ca
Matías Bustos <agustinbustos_com@hotmail.com>
2023-08-02 23:12:42 +0200

    Applies coding stlye rules on existing JS files

--- 01-perlin-noise/bouncing-ball/sketch.js
+++ 01-perlin-noise/bouncing-ball/sketch.js
@@ -1,15 +1,15 @@
-let xOffset = 0;
-let yOffset = 1000;
+let xOffset = 0
+let yOffset = 1000
 
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(400, 400)
 }
 
 function draw() {
-  xOffset+=0.01;
-  yOffset+=0.01;
-  const x = noise(xOffset) * width;
-  const y = noise(yOffset) * height;
-  background(51);
-  ellipse(x, y, 24, 24);
+    xOffset += 0.01
+    yOffset += 0.01
+    const x = noise(xOffset) * width
+    const y = noise(yOffset) * height
+    background(51)
+    ellipse(x, y, 24, 24)
 }
